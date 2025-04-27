99d0d3ca7d7ee435536252c5b6c20539777c6011
dallatIkes <samy.chaabi0@gmail.com>
2025-04-27 23:08:17 +0200

    added horizontal navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ import {
 
 // Importing the style
 import './styles/App.css'
+import './styles/navbar.css'
 
 // Importing all the pages
 import About from './pages/About.jsx'
 import Cart from './pages/Cart.jsx'
-import Catalaog from './pages/Catalog.jsx'
+import Catalog from './pages/Catalog.jsx'
 import Contact from './pages/Contact.jsx'
 import Home from './pages/Home.jsx'
 import HowItWorks from './pages/HowItWorks.jsx'
@@ -24,65 +25,45 @@ import Register from './pages/Register.jsx'
 import Sponsor from './pages/Sponsor.jsx'
 
 function App() {
-  return (
-      <Router>
-          <nav>
-              <ul>
-                  <li>
-                      <Link to="/about">Qui somme-nous ?</Link>
-                  </li>
-                  <li>
-                      <Link to="/how-it-works">Comment ça marche ?</Link>
-                  </li>
-                  <li>
-                        Commander
-                        <ul>
-                            <li>
-                                 <Link to="/catalog">Catalogue de porduits</Link>
-                            </li>
-                            <li>
-                                 <Link to="/cart">Mon panier</Link>
-                            </li>
-                        </ul>
-                  </li>
-                  <li>
-                      <Link to="/more">Toujours plus</Link>
-                  </li>
-                  <li>
-                      <Link to="/contact">Nous contacter</Link>
-                  </li>
-                  <li>
-                      <Link to="/register">Inscription</Link>
-                  </li>
-                  <li>
-                      <Link to="/login">Connection</Link>
-                  </li>
-                  <li>
-                      <Link to="/sponsor">Sponsors</Link>
-                  </li>
-                  <li>
-                      <Link to="/news">Actualité</Link>
-                  </li>
-              </ul>
+    return (
+        <Router>
+          <nav className="navbar">
+            <ul className="navbar-menu">
+              <li><Link to="/about">Qui sommes-nous ?</Link></li>
+              <li><Link to="/how-it-works">Comment ça marche ?</Link></li>
+              
+              <li className="dropdown">
+                <span className="dropdown-title">Commander</span>
+                <ul className="dropdown-menu">
+                  <li><Link to="/catalog">Catalogue de produits</Link></li>
+                  <li><Link to="/cart">Mon panier</Link></li>
+                </ul>
+              </li>
+    
+              <li><Link to="/more">Toujours plus</Link></li>
+              <li><Link to="/contact">Nous contacter</Link></li>
+              <li><Link to="/register">Inscription</Link></li>
+              <li><Link to="/login">Connexion</Link></li>
+              <li><Link to="/sponsor">Sponsors</Link></li>
+              <li><Link to="/news">Actualité</Link></li>
+            </ul>
           </nav>
-
-          {/*Implementing Routes for respective Path */}
-
+    
           <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />}/>
-              <Route path="/cart" element={<Cart />}/>
-              <Route path="/catalog" element={<Catalaog />}/>
-              <Route path="/contact" element={<Contact />}/>
-              <Route path="/how-it-works" element={<HowItWorks />}/>
-              <Route path="/login" element={<Login />}/>
-              <Route path="/more" element={<More />}/>
-              <Route path="/news" element={<News />}/>
-              <Route path="/register" element={<Register />}/>
-              <Route path="/sponsor" element={<Sponsor />}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/how-it-works" element={<HowItWorks />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/more" element={<More />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/sponsor" element={<Sponsor />} />
           </Routes>
-      </Router>
-  );
+        </Router>
+      );
 }
 
 export default App
