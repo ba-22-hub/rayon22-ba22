51f8c1aed2fc966a8a2436c8eb10d6704b629578
dallatIkes <samy.chaabi0@gmail.com>
2025-04-28 15:37:22 +0200

    added the About page text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,8 +8,20 @@ import LoremIpsum from "../common/LoremIpsum"
 function About() {
     return (
         <>
-            <h1>This is the About Us Page</h1>
-            <LoremIpsum></LoremIpsum>
+            <h1>Les banques alimentaires, premier réseau de distribution d’aide alimentaire en France</h1>
+            <p>
+                Le Rayon 22 est une épicerie sociale et solidaire dont l’objectif est d’accompagner les personnes en difficulté financière en leur donnant accès à une alimentation à petits prix sur tout le territoire des Côtes d’Armor.
+
+                Le Rayon 22 permet aussi de donner accès à l’aide alimentaire aux personnes qui ne peuvent se rendre aux distributions de nos partenaires.
+
+                Pour accéder à notre épicerie en ligne en point relais, il faut vous connecter à un compte.
+            </p>
+            <h2>La banque alimentaire des côtes d’Armor</h2>
+            <p>
+                La banque alimentaire des côtes d’Armor a été créée le 16 octobre 1984,  elle est pionnière en la matière ! 
+
+                Avec le Rayon 22 Epicerie en ligne, la Banque Alimentaire des Côtes d’Armor innove dans le domaine de la distribution alimentaire
+            </p>
         </>
     )
 }
