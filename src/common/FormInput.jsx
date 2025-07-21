b9cf0a29ae2ac41c63b812c658cd1b387eb47944
dallatIkes <samy.chaabi0@gmail.com>
2025-07-21 18:50:38 +0200

    fixed components className

--- src/common/FormInput.jsx
+++ src/common/FormInput.jsx
@@ -1,17 +1,18 @@
 /**
  * A basic string input.
  * @param {Object} props - The component props.
+ * @param {string} props.className - The Tailwind class to apply to the input.
  * @param {string} props.inputText - The input label content.
  * @param {string} props.name - The field name in the JSON object.
  * @param {string} props.value - The value entered by the customer.
  * @param {(e: any) => void} props.onChange - The function to call whenever the value changes.
  * @returns {React.ReactElement} FormInput component.
  */
-function FormInput({ inputText, name, value, onChange }) {
+function FormInput({ className, inputText, name, value, onChange }) {
     return (
         <div>
             <div><label>{inputText}</label></div>
-            <div><input className="border border-[#2E2EFF] rounded-md text-sm px-4 py-2 w-full" type="text" name={name} value={value} onChange={onChange} /></div>
+            <div><input className={className} type="text" name={name} value={value} onChange={onChange} /></div>
         </div>
     );
 }
--- src/common/FormTextArea.jsx
+++ src/common/FormTextArea.jsx
@@ -1,17 +1,18 @@
 /**
  * A basic textbox input.
  * @param {Object} props - The component props. 
+ * @param {string} props.className - The Tailwind class to apply to the textarea.
  * @param {string} props.textAreaName - The input label content.
  * @param {string} props.name - The field name in the JSON object.
  * @param {string} props.value - The value entered by the customer.
  * @param {(e: any) => void} props.onChange - The function to call whenever the value changes.
  * @returns {React.ReactElement} FormTextArea component.
  */
-function FormTextArea({ textAreaName, name, value, onChange }) {
+function FormTextArea({ className, textAreaName, name, value, onChange }) {
     return (
         <>
             <label>{textAreaName}</label><br />
-            <textarea className="h-64 border border-[#2E2EFF] rounded-md text-sm px-4 py-2 w-full" name={name} value={value} onChange={onChange} style={{resize: "none"}} /><br /><br />
+            <textarea className={className} name={name} value={value} onChange={onChange} style={{resize: "none"}} /><br /><br />
         </>
     );
 }
