a2c1ae6aab53db76432b9397c780d24b4a9dbcc3
gwen <login@enssat.fr>
2025-07-22 13:01:19 +0200

    swtiching registerstep3 to tailwind

--- src/pages/RegisterStep3.jsx
+++ src/pages/RegisterStep3.jsx
@@ -10,12 +10,12 @@ function RegisterStep3({mail}) {
 
     return (
         <>
-            <div className='img-center'>
-                <img src={imageMail}/>
+            <div className='flex items-center w-[100%] justify-center mt-[5%] '>
+                <img className="w-[40%] h-[40%]" src={imageMail}/>
             </div>
-            <h2 className='center'>Email de confirmation</h2>
-            <p className='center'><black>Un email de confirmation a été envoyé à  </black><span className="email">{mail}</span><black> afin de vérifier la validité de l'adresse. Une fois reçu, veuillez cliquer sur le lien pour finaliser votre inscription</black></p>
-            <button className='register-button'>Re-envoyer le mail</button>
+            <h2 className='text-center text-rayonblue text-[3.2em] leading-tight pt-[2%] font-bold'>Email de confirmation</h2>
+            <p className='text-center mr-[8%] ml-[8%] mt-8'><black>Un email de confirmation a été envoyé à  </black><span className="email">{mail}</span><black> afin de vérifier la validité de l'adresse. Une fois reçu, veuillez cliquer sur le lien pour finaliser votre inscription</black></p>
+            <button className='text-center w-[50%] ml-[25%] mb-[3%] mt-[5%] h-[2rem]'>Re-envoyer le mail</button>
         </>
     ) 
 }
