964b5915f87410ba04cea601cc9fde38e0b46757
gwen <login@enssat.fr>
2025-07-25 19:23:38 +0200

    adding function to edit profile

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,3 +1,5 @@
+import { Input } from "postcss"
+import { useState } from "react"
 
 
 
@@ -22,42 +24,144 @@ function Account({ client }) {
      * wageType: undefined
      */
 
-    const fullAddress = client.address + ' ' + client.addAddress + ', ' + client.postalCode + ' ' + client.city
 
+
+
+    const [editing, setEditing] = useState(false)
+    const [clientEdit, setClientEdit] = useState(client)
+    const genderOptions = ["male", "female", "other"]
+    const situationOptions = ["employee", "jobless", "student", "retired"]
+    const wageOptions = ["salary", "scholarship", "help", "other"]
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        if (name == "wageType" && value == "other") {
+            const other = window.prompt("Quel est votre type de salaire ?");
+            if (other) {
+                setClientEdit(prev => ({
+                    ...prev,
+                    [name]: value,
+                    otherWage: other
+                }));
+
+            }
+        } else {
+            setClientEdit(prev => ({
+                ...prev,
+                [name]: value
+            }))
+        }
+    }
+
+    function handleCancel() {
+        setClientEdit(client)
+        setEditing(false)
+        console.log("editmod disable")
+    }
+
+    function handleEdit() {
+        console.log("sending ", clientEdit, "to API...")
+        setEditing(false)
+    }
+
+    // on factorise l'élément le plus volumineux 
+    const renderField = (label, fieldName) => (
+        <div className="flex flex-row text-rayonblue">
+            <label className="font-semibold w-[23%] mt-2 mb-2">{label} : </label>
+            {editing ? (
+                <input
+                    className="ml-3 border border-rayonorange rounded-lg w-[78%] mt-1 mb-1 text-rayonorange"
+                    name={fieldName}
+                    value={clientEdit[fieldName]}
+                    onChange={handleChange}
+                />
+            ) : (
+                <p className="ml-3 mt-2 mb-2">{clientEdit[fieldName]}</p>
+            )}
+        </div>
+    )
+
+    const renderRadio = (label, fieldName, options) => (
+        <div className="flex flex-row text-rayonblue mb-2">
+            <label className="font-semibold w-[23%] mt-2 mb-2">{label} :</label>
+            {editing ? (
+                <div className="flex text-rayonorange">
+                    {options.map((option) => (
+                        <label key={option} className="flex items-center ml-4">
+                            <input
+                                className="mr-1"
+                                type="radio"
+                                name={fieldName}
+                                value={option}
+                                checked={clientEdit[fieldName] === option}
+                                onChange={handleChange}
+                            />
+                            {option}
+                        </label>
+                    ))}
+                </div>
+            ) : (
+                <p className="ml-3 mt-2 mb-2">{clientEdit[fieldName]}</p>
+            )}
+        </div>
+    );
 
     return (
         <>
             <div className="w-[66%] ml-[17%] p-[8%] bg-white rounded-2xl shadow-sm mb-[4%]">
                 <h1 className="text-center text-rayonblue text-[4.3em] leading-tight pt-[2%] font-bold">Bienvenue sur votre Espace Utilisateur</h1>
                 <div className="flex flex-row">
-                    <div className="border border-rayonblue rounded-lg mt-[1.5em] w-[48%] p-2">
+                    <div className="border border-rayonblue rounded-lg mt-[1.5em] w-[49%] p-2">
                         <h2 className="text-rayonblue text-[1.5em] font-semibold">État civil</h2>
-                        <div className="flex flex-row text-rayonblue"><label className="font-semibold">Nom : </label><p className="ml-3">{client.lastName}</p></div>
-                        <div className="flex flex-row text-rayonblue"><label className="font-semibold">Prénom : </label><p className="ml-3">{client.firstName}</p></div>
-                        <div className="flex flex-row text-rayonblue"><label className="font-semibold">Genre : </label><p className="ml-3">{client.gender}</p></div>
+                        {renderField("Nom", "lastName")}
+                        {renderField("Prénom", "firstName")}
+                        {renderRadio("Genre", "gender", genderOptions)}
+
                     </div>
-                    <div className="border border-rayonblue rounded-lg mt-[1.5em] w-[48%] ml-[4%] p-2">
+                    <div className="border border-rayonblue rounded-lg mt-[1.5em] w-[49%] ml-[2%] p-2">
                         <h2 className="text-rayonblue text-[1.5em] font-semibold">Contact</h2>
-                        <div className="flex flex-row text-rayonblue"><label className="font-semibold">E-mail : </label><p className="ml-3">{client.email}</p></div>
-                        <div className="flex flex-row text-rayonblue"><label className="font-semibold">Téléphone : </label><p className="ml-3">{client.phone}</p></div>
-                        <div className="flex flex-row text-rayonblue"><label className="font-semibold">Addresse : </label><p className="ml-3">{fullAddress}</p></div>
+                        {renderField("E-mail", "email")}
+                        {renderField("Téléphone", "phone")}
+                        {renderField("Adresse", "address")}
+                        {renderField("Précisions", "addAddress")}
+                        {renderField("Ville", "city")}
+                        {renderField("Code postal", "phone")}
                     </div>
                 </div>
                 <div className="border border-rayonblue rounded-lg mt-[1.5em] w-[100%] p-2">
                     <h2 className="text-rayonblue text-[1.5em] font-semibold">Déclarations</h2>
-                    <div className="flex flex-row text-rayonblue"><label className="font-semibold">Situation : </label><p className="ml-3">{client.situation}</p></div>
-                    <div className="flex flex-row text-rayonblue"><label className="font-semibold">Type de salaire : </label><p className="ml-3">{client.wageType}</p></div>
-                    <div className="flex flex-row text-rayonblue"><label className="font-semibold">Quotient familial (CAF) : </label><p className="ml-3">{client.quotient}</p></div>
-
+                    {renderRadio("Situation", "situation", situationOptions)}
+                    {renderField("Quotient familial (CAF)", "quotient")}
+                    {renderRadio("Type de salaire", "wageType", wageOptions)}
                 </div>
                 <div className="border border-rayonblue rounded-lg mt-[1.5em] w-[100%] p-2">
                     <h2 className="text-rayonblue text-[1.5em] font-semibold">Vos droits</h2>
-                    <div className="flex flex-row text-rayonblue"><label className="font-semibold">Date de validité du compte : </label><p className="ml-3">{}</p></div>
-                    <div className="flex flex-row text-rayonblue"><label className="font-semibold">Limite de commande mensuelle : </label><p className="ml-3">{}</p></div>
-                    <div className="flex flex-row text-rayonblue"><label className="font-semibold">Reste à commander : </label><p className="ml-3">{}</p></div>
+                    <div className="flex flex-row text-rayonblue"><label className="font-semibold">Date de validité du compte : </label><p className="ml-3">{ }</p></div>
+                    <div className="flex flex-row text-rayonblue"><label className="font-semibold">Limite de commande mensuelle : </label><p className="ml-3">{ }</p></div>
+                    <div className="flex flex-row text-rayonblue"><label className="font-semibold">Reste à commander : </label><p className="ml-3">{ }</p></div>
                 </div>
-                <button className="text-white text-center bg-rayonorange w-[50%] ml-[25%] mb-3 mt-[10%] h-[2rem]">Modifier 🖉</button>
-
+                {!editing ? (
+                    <button
+                        className="text-white text-center bg-rayonorange w-[50%] ml-[25%] mb-3 mt-[10%] h-[2rem]"
+                        onClick={() => {
+                            setEditing(true)
+                            console.log("editmod enabled")
+                        }
+                        }
+                    >Modifier 🖉</button>
+                ) : (
+                    <div className="flex flex-row">
+                        <button
+                            className="text-white text-center bg-rayonorange w-[24%] ml-[25%] mb-3 mt-[10%] h-[2rem]"
+                            onClick={handleCancel}
+                        >Annuler ✖</button>
+                        <button
+                            className="text-rayonorange text-center bg-white w-[24%] ml-[2%] mb-3 mt-[10%] h-[2rem] border border-rayonorange"
+                            onClick={handleEdit}
+                        >Valider 🗸</button>
+                    </div>
+                )
+                }
             </div>
         </>
     )
@@ -66,4 +170,5 @@ function Account({ client }) {
 
 }
 
-export default Account
+
+export default Account;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -99,7 +99,7 @@ function Login() {
     </div>
     <div>
       <Account 
-        client={{birthday: "0023-09-23",  email: "no@martin.bzh", firstName: "Nolwenn", gender: "female", lastName: "Martin", phone: "0943439843", acceptTerms: true, addAddress: "", address: "25 rue de la lune ", city: "Brest", otherWage: "", postalCode: "29000", quotient: "un certain nombre", readInfo: true, situation: "jobless", wageType: undefined}}/>
+        client={{birthday: "0023-09-23",  email: "no@martin.bzh", firstName: "Nolwenn", gender: "female", lastName: "Martin", phone: "0943439843", acceptTerms: true, addAddress: "", address: "25 rue de la lune ", city: "Brest", otherWage: "", postalCode: "29000", quotient: "un certain nombre", readInfo: true, situation: "jobless", wageType: "salary"}}/>
     </div>
   </>
 )
