60ea38c53f1c00a38e3c36f77a2573b35381ec6e
gwen <login@enssat.fr>
2025-07-30 12:58:42 +0200

    adding the files is better...

---
+++ frontend/src/context/AuthorContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import {supabase} from "../../lib/supabaseClient.js"
+
+
+const AuthorContext = createContext()
+
+function AuthorProvider({ children }){
+
+    const [user, setUser] = useState(null)
+
+    // check if a session is already open
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data }) => {
+            setUser(data.session?.user ?? null) // if exists return data.sessions.user, else user = null
+        })
+
+
+        // listen author changes 
+        const { data: listener } = supabase.auth.onAuthStateChange((_, session) => {
+            setUser(session?.user ?? null)
+        })
+
+        // when the user close the app 
+        return () => {
+            listener.subscription.unsubscribe()
+        }
+
+
+    }, [])
+
+    return (
+        <AuthorContext.Provider value={{ user, setUser }}>
+            {children}
+        </AuthorContext.Provider>
+    );
+
+}
+
+const useAuthor = () => useContext(AuthorContext)
+
+export {AuthorProvider, useAuthor}
