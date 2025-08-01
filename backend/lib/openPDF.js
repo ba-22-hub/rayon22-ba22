20354b935f2688a60a8bb17fa58ded406f2fc8a4
gwen <login@enssat.fr>
2025-08-01 07:55:17 +0200

    ceating openPDF function, working with signedURL

---
+++ backend/lib/openPDF.js
@@ -0,0 +1,22 @@
+import { supabase } from '@lib/supabaseClient.js';
+
+
+// durée de base : 10*60s = 10min 
+async function openPDF(fileName, expiresIn = 10) {
+    console.log("tentative d'ouverture du fichier : ", fileName)
+
+    const { data, error } = await supabase.storage
+        .from('documents')
+        .createSignedUrl(fileName, expiresIn * 60)
+
+    if (error) {
+        console.error("Une erreur est survenue lors de la génération de l'URL : ", error)
+    }
+
+    if (data?.signedUrl) {
+        // Ouvre le PDF dans un nouvel onglet
+        window.open(data.signedUrl, '_blank', 'noopener,noreferrer');
+    } else {
+        console.error("Impossible de générer une URL signée pour ce fichier.");
+    }
+}
