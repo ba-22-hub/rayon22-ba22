8730385908e6bb8e5ec990ff40bc8288d68e98ef
dallatIkes <samy.chaabi0@gmail.com>
2025-08-05 22:26:15 +0100

    added build tools

---
+++ electron/main.js
@@ -0,0 +1,38 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'), // adapte ce chemin si besoin
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    })
+
+    if (import.meta.env?.DEV) {
+        mainWindow.loadURL('http://localhost:5173')
+    } else {
+        mainWindow.loadFile(path.join(__dirname, 'dist/index.html'))
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
