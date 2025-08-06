53475d57a137a1972f1b8a83e2293d402542437f
dallatIkes <samy.chaabi0@gmail.com>
2025-08-06 23:45:57 +0100

    [bugFix] - Contact page is now accessible

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,6 +11,7 @@ import FormTextArea from "../common/FormTextArea"
 
 // Importing assets
 import roundLogo from "../assets/logos/roundLogo.png"
+import Loading from '../common/Loading';
 
 /**
  * The Contact page.
@@ -33,11 +34,8 @@ function Contact() {
 		if (!user) {
 			alert("Vous devez vous connecter pour utiliser cette fonctionnalité !")
 			navigate('/login')
-
+			return;
 		}
-
-
-
 	}, [loading]) // useEffect trigger again when all is loaded
 
 
@@ -121,68 +119,62 @@ function Contact() {
 	}
 	return (
 		<>
-			{user ? (
-				<p>Chargement...</p>
-			) : (
-				<>
-					<div className="bg-gradient-to-b from-[#3435FF] via-[#2526B7] to-[#1F2099] h-52 text-white flex items-center justify-center">
-						<h1 className="text-5xl font-bold">Contactez-nous</h1>
-					</div>
+			<div className="bg-gradient-to-b from-[#3435FF] via-[#2526B7] to-[#1F2099] h-52 text-white flex items-center justify-center">
+				<h1 className="text-5xl font-bold">Contactez-nous</h1>
+			</div>
 
-					<div className="flex justify-center bg-[#FFF8F4] pb-20">
-						{/* Left Text Section */}
-						<div className="w-[35%] mt-24 pr-16">
-							<h2 className="text-[#2E2EFF] text-3xl font-bold mb-6">Un contact si besoin</h2>
-							<p className="text-[#2E2EFF] text-xl leading-relaxed">
-								Quelque soit le sujet (une réclamation, un problème de livraison ou de délais, un contenu défectueux...)
-								ou un sujet concernant ma situation personnelle, j’adresse un message (pour un problème de commande
-								préciser la date de commande).
-								<br /><br />
-								Je serai recontacté au numéro de téléphone ou l’adresse mail donné lors de mon inscription.
-							</p>
-							<img src={roundLogo} alt="Logo" className="w-80 mt-16 ml-12 -rotate-[13.55deg]" />
+			<div className="flex justify-center bg-[#FFF8F4] pb-20">
+				{/* Left Text Section */}
+				<div className="w-[35%] mt-24 pr-16">
+					<h2 className="text-[#2E2EFF] text-3xl font-bold mb-6">Un contact si besoin</h2>
+					<p className="text-[#2E2EFF] text-xl leading-relaxed">
+						Quelque soit le sujet (une réclamation, un problème de livraison ou de délais, un contenu défectueux...)
+						ou un sujet concernant ma situation personnelle, j’adresse un message (pour un problème de commande
+						préciser la date de commande).
+						<br /><br />
+						Je serai recontacté au numéro de téléphone ou l’adresse mail donné lors de mon inscription.
+					</p>
+					<img src={roundLogo} alt="Logo" className="w-80 mt-16 ml-12 -rotate-[13.55deg]" />
+				</div>
+
+				{/* Right Form Section */}
+				<div className="bg-white shadow-md rounded-xl px-10 py-8 w-[45%] h-[45%] mt-16">
+					<h2 className="text-[#2E2EFF] text-2xl font-bold mb-6">Formulaire de contact</h2>
+
+					<form onSubmit={handleSubmit}>
+
+						{/* Message */}
+						<FormTextArea
+							textAreaName={"Message"}
+							name="message"
+							value={formData.message}
+							onChange={handleChange}
+							isStarred={true}
+							className="h-64 border border-[#2E2EFF] rounded-md text-sm px-4 py-2 w-full"
+						/>
+
+						{/* File Upload */}
+						<label className="block text-sm font-medium text-[#2E2EFF] mb-2">Document requis :</label>
+						<input
+							type='file'
+							accept='.pdf'
+							name='file'
+							onChange={(e) => { console.log("Fichier sélectionné :", e.target.files[0]); handleFileChange(e); }}
+							ref={fileInputRef}
+							className="mb-6 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-[#2E2EFF] file:text-white hover:file:bg-blue-700"
+						/>
+
+						<div className="flex justify-center">
+							<button
+								type="submit"
+								className="bg-[#FF7A00] text-white font-light tracking-wider w-full py-3 rounded-lg text-sm hover:bg-orange-600 transition"
+							>
+								Envoyer
+							</button>
 						</div>
-
-						{/* Right Form Section */}
-						<div className="bg-white shadow-md rounded-xl px-10 py-8 w-[45%] h-[45%] mt-16">
-							<h2 className="text-[#2E2EFF] text-2xl font-bold mb-6">Formulaire de contact</h2>
-
-							<form onSubmit={handleSubmit}>
-
-								{/* Message */}
-								<FormTextArea
-									textAreaName={"Message"}
-									name="message"
-									value={formData.message}
-									onChange={handleChange}
-									isStarred={true}
-									className="h-64 border border-[#2E2EFF] rounded-md text-sm px-4 py-2 w-full"
-								/>
-
-								{/* File Upload */}
-								<label className="block text-sm font-medium text-[#2E2EFF] mb-2">Document requis :</label>
-								<input
-									type='file'
-									accept='.pdf'
-									name='file'
-									onChange={(e) => { console.log("Fichier sélectionné :", e.target.files[0]); handleFileChange(e); }}
-									ref={fileInputRef}
-									className="mb-6 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-[#2E2EFF] file:text-white hover:file:bg-blue-700"
-								/>
-
-								<div className="flex justify-center">
-									<button
-										type="submit"
-										className="bg-[#FF7A00] text-white font-light tracking-wider w-full py-3 rounded-lg text-sm hover:bg-orange-600 transition"
-									>
-										Envoyer
-									</button>
-								</div>
-							</form>
-						</div>
-					</div>
-				</>
-			)}
+					</form>
+				</div>
+			</div>
 		</>
 	)
 }
