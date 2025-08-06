187a98b3d134c6d9d48ddbf8708f9d9e8ce612c3
gwen <login@enssat.fr>
2025-08-06 12:31:50 +0200

    adding logout button for admin

--- src/AdminApp.jsx
+++ src/AdminApp.jsx
@@ -4,7 +4,9 @@ import {
   Route,
   Link,
   Outlet,
+  useNavigate
 } from "react-router-dom";
+import { useAuthor } from "./context/AuthorContext.jsx";
 
 import AdminLogin from "./pages/dashboard/AdminLogin.jsx";
 import UserTable from "./pages/dashboard/UserTable.jsx";
@@ -13,14 +15,30 @@ import MessagesDashboard from "./pages/dashboard/MessagesDashboard.jsx";
 import RequestsDashboard from "./pages/dashboard/RequestsDashboard.jsx";
 
 function AdminNavbar() {
+  const {logout} = useAuthor()
+  const navigate = useNavigate()
+  function handleLogout(){
+    logout()
+    navigate('/admin')
+  }
+
   return (
-    <nav className="bg-rayonblue text-white p-4 flex justify-between">
+    <nav className="bg-rayonblue text-white p-4 flex justify-between items-center">
+      {/* Liens de gauche */}
       <div className="space-x-4">
         <Link to="/admin/users" className="hover:underline">Utilisateurs</Link>
         <Link to="/admin/products" className="hover:underline">Produits</Link>
         <Link to="/admin/messages" className="hover:underline">Messages</Link>
         <Link to="/admin/requests" className="hover:underline">Demandes</Link>
       </div>
+
+      {/* Bouton de déconnexion */}
+      <button
+        onClick={handleLogout}
+        className="bg-red hover:bg-red-600 px-4 py-2 rounded-2xl font-bold"
+      >
+        ⏼
+      </button>
     </nav>
   );
 }
