ee09807dbd3295cc6d6c2964ece1be5cbbb4c10b
dallatIkes <samy.chaabi0@gmail.com>
2025-08-18 11:57:00 +0100

    [bugFix] - data.path instead of raw data in notification

--- src/lib/sendPDF.js
+++ src/lib/sendPDF.js
@@ -1,6 +1,13 @@
+// Importing dependencies
 import { supabase } from './supabaseClient.js';
 import { displayNotification } from '@lib/displayNotification.js'
 
+/** * Uploads a PDF file to Supabase storage.
+ * @param {File} file - The PDF file to upload.
+ * @param {string} fileName - The name of the file to be uploaded.
+ * @param {string} folder - The folder in the Supabase bucket where the file will be stored.
+ * @returns {Promise<{ success: boolean, data?: object, error?: object }>} - A promise that resolves to an object indicating success or failure of the upload operation.
+ */
 async function uploadPDF(file, fileName, folder) {
     console.log('Uploading file:', fileName, { upsert: false });
 
@@ -20,7 +27,7 @@ async function uploadPDF(file, fileName, folder) {
         return { success: false, error: uploadError };;
     }
 
-    displayNotification("Upload terminé avec succès", uploadData, "success")
+    displayNotification("Upload terminé avec succès", `Chemin : ${uploadData.path}`, "success")
     return { success: true, data: uploadData };
 }
 
