07b48b9b299eafbd6e1208c8d18838038748c298
gwen <login@enssat.fr>
2025-08-18 15:55:15 +0200

    now the transition to cart just after login seems to be fine

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -36,7 +36,7 @@ function Cart() {
     const [shippingCost, setShippingCost] = useState(1)
     const isNotified = useRef(false)
 
-    const { user, loading, hasRights } = useAuthor()
+    const { user, loading, hasRights, checkHasRights } = useAuthor()
     const { cart, setCart } = useCart()
 
     let navigate = useNavigate()
@@ -48,10 +48,15 @@ function Cart() {
         if (!user) { // user not login 
             navigate('/login')
             notify("Vous devez être connectés et avoir les droits pour passer commande")
-        }
-        else if (!hasRights) { // user doesn't have rights
-            notify("Vous n'avez pas (encore ?) les droits. Pour passer une commande, veuillez déposer un fichier dans votre espace compte")
-            navigate('/account') // send the user to the previous page
+        } else {
+            checkHasRights(user.id) // user doesn't have rights
+                .then((rights) => {
+                    if(!rights){
+                        notify("Vous n'avez pas (encore ?) les droits. Pour passer une commande, veuillez déposer un fichier dans votre espace compte")
+                        navigate('/account') 
+                    }
+                })
+            
         }
         // end author routine 
 
