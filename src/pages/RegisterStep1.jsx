c24888bb06e3bb02a786d2ee5d2f5835e729b99b
gwen <login@enssat.fr>
2025-10-14 14:40:58 +0200

    registerstep 3 & 4

--- src/pages/RegisterStep1.jsx
+++ src/pages/RegisterStep1.jsx
@@ -74,10 +74,10 @@ function RegisterStep1({ data, onDataChange, onNext }) {
                 <FormInput labelClassName="ml-[8%]" className="w-[84%] h-[2.3rem] ml-[8%] rounded-lg border border-rayonblue mb-2 mt-1" inputText="Téléphone" name="phone" value={formData.phone} onChange={handleChange} isStarred={true} />
                 <FormInput labelClassName="ml-[8%]" className="w-[84%] h-[2.3rem] ml-[8%] rounded-lg border border-rayonblue mb-2 mt-1" inputText="Adresse mail" name="email" value={formData.email} onChange={handleChange} isStarred={true} />
 
-                <button type="submit" className='text-center-white bg-rayonorange w-[50%] ml-[25%] mb-3 mt-[10%] h-[2rem]'>Suivant</button>
+                <button type="submit" className='text-center-white bg-rayonorange w-[80%] ml-[10%] lg:w-[50%] lg:ml-[25%] mb-3 mt-[10%] h-[2rem]'>Suivant</button>
             </form>
             <PageButton
-                className='text-center text-rayonorange bg-white w-[50%] ml-[25%] mb-[4%] h-[2rem] rounded-lg border border-rayonorange '
+                className='text-center text-rayonorange bg-white w-[80%] ml-[10%] lg:w-[50%] lg:ml-[25%] mb-[4%] h-[2rem] rounded-lg border border-rayonorange '
                 buttonText="J'ai déjà un compte"
                 page="../Login"
             />
