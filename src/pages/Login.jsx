4686ed95c1acbaaa218ad3778130546f08a309aa
gwen <login@enssat.fr>
2025-11-21 20:35:05 +0100

    indentation changes...

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -120,73 +120,72 @@ function Login() {
 
 	}
 
-	return (
-		<>
-			<div className="bg-[#ffffff] w-[65.56vw] mx-auto mt-32 mb-10 rounded-2xl shadow-sm py-12 px-6">
-				<h1 className="text-[#2E2EFF] text-7xl font-extrabold text-center leading-tight mb-2">
-					Bienvenue sur votre Espace Utilisateur
-				</h1>
-				<p className="text-black text-base text-center mb-10 mt-4">
-					Connectez vous en utilisant le formulaire ci-dessous
-				</p>
-				<form onSubmit={handleSubmit} className="space-y-6">
-					<div className="w-[65%] mx-auto">
-						<FormInput
-							inputText={<span className="text-rayonblue">Adresse email</span>}
-							name={'mail'}
-							value={formData.mail}
-							onChange={handleChange}
-							isStarred={true}
-							className="border border-[#2E2EFF] rounded-md text-sm px-4 py-2 w-full"
-						/>
-					</div>
-					<div className="w-[65%] mx-auto">
-						<FormInput
-							inputText={<span className="text-rayonblue">Mot de passe</span>}
-							name={'password'}
-							value={formData.password}
-							onChange={handleChange}
-							isStarred={true}
-							type="password"
-							className="border border-[#2E2EFF] rounded-md text-sm px-4 py-2 w-full"
-						/>
-					</div>
-					<div className="text-right w-[65%] mx-auto">
-						<PageButton
-							buttonText={'Mot de passe oublié ?'}
-							page={'/forgot-password'}
-							className="text-[#2E2EFF] text-sm font-medium underline hover:text-blue-600"
-						/>
-					</div>
-					<div className="w-full flex justify-center">
-						<PageButton
-							buttonText={'Je me connecte'}
-							type="submit"
-							className="w-[400px] h-10 bg-[#FF8200] text-white rounded-lg text-sm font-medium hover:bg-orange-600 transition"
-						/>
-					</div>
-				</form>
-
-				<p className="text-[#2E2EFF] text-sm text-center mt-10 mb-4 font-medium">
-					Vous n'avez pas encore de compte ?
-				</p>
-				<div className="flex justify-center">
+return (
+	<>
+		<div className="bg-[#ffffff] w-[65.56vw] mx-auto mt-32 mb-10 rounded-2xl shadow-sm py-12 px-6">
+			<h1 className="text-[#2E2EFF] text-7xl font-extrabold text-center leading-tight mb-2">
+				Bienvenue sur votre Espace Utilisateur
+			</h1>
+			<p className="text-black text-base text-center mb-10 mt-4">
+				Connectez vous en utilisant le formulaire ci-dessous
+			</p>
+			<form onSubmit={handleSubmit} className="space-y-6">
+				<div className="w-[65%] mx-auto">
+					<FormInput
+						inputText={<span className="text-rayonblue">Adresse email</span>}
+						name={'mail'}
+						value={formData.mail}
+						onChange={handleChange}
+						isStarred={true}
+						className="border border-[#2E2EFF] rounded-md text-sm px-4 py-2 w-full"
+					/>
+				</div>
+				<div className="w-[65%] mx-auto">
+					<FormInput
+						inputText={<span className="text-rayonblue">Mot de passe</span>}
+						name={'password'}
+						value={formData.password}
+						onChange={handleChange}
+						isStarred={true}
+						type="password"
+						className="border border-[#2E2EFF] rounded-md text-sm px-4 py-2 w-full"
+					/>
+				</div>
+				<div className="text-right w-[65%] mx-auto">
 					<PageButton
-						buttonText={'Créez votre compte'}
-						page={'/register'}
+						buttonText={'Mot de passe oublié ?'}
+						page={'/forgot-password'}
+						className="text-[#2E2EFF] text-sm font-medium underline hover:text-blue-600"
+					/>
+				</div>
+				<div className="w-full flex justify-center">
+					<PageButton
+						buttonText={'Je me connecte'}
+						type="submit"
 						className="w-[400px] h-10 bg-[#FF8200] text-white rounded-lg text-sm font-medium hover:bg-orange-600 transition"
 					/>
 				</div>
-				<div className="flex justify-center">
-					<PageButton
-						buttonText='Admin'
-						page='/admin'
-						className='w-[400px] h-10 bg-[#FF8200] text-white rounded-lg text-sm font-medium hover:bg-orange-600 transition mt-4' />
-				</div>
-			</div>
+			</form>
 
-		</>
-	)
+			<p className="text-[#2E2EFF] text-sm text-center mt-10 mb-4 font-medium">
+				Vous n'avez pas encore de compte ?
+			</p>
+			<div className="flex justify-center">
+				<PageButton
+					buttonText={'Créez votre compte'}
+					page={'/register'}
+					className="w-[400px] h-10 bg-[#FF8200] text-white rounded-lg text-sm font-medium hover:bg-orange-600 transition"
+				/>
+			</div>
+			<div className="flex justify-center">
+				<PageButton
+					buttonText='Admin'
+					page='/admin'
+					className='w-[400px] h-10 bg-[#FF8200] text-white rounded-lg text-sm font-medium hover:bg-orange-600 transition mt-4' />
+			</div>
+		</div>
+	</>
+)
 
 }
 
