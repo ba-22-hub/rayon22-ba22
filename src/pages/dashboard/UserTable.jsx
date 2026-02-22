00d6485148aa408391b87919bbcb674b55881fba
gwen <login@enssat.fr>
2026-02-22 01:27:20 +0100

    creating front for a modal to add users, need to connect to db

--- src/pages/dashboard/UserTable.jsx
+++ src/pages/dashboard/UserTable.jsx
@@ -12,6 +12,7 @@ import { displayNotification } from '@lib/displayNotification.jsx';
 // Importing common components
 import FunctionButton from '@common/FunctionButton.jsx';
 import Loading from '@common/Loading.jsx';
+import AddUserModal from '../../common/AddUserModal';
 
 
 const UserTable = () => {
@@ -22,6 +23,7 @@ const UserTable = () => {
 	const [editedUser, setEditedUser] = useState({});
 	const [update, setUpdate] = useState(true)
 	const [isLoading, setIsLoading] = useState(true);
+	const [modalOpen, setModalOpen] = useState(false)
 
 	const { isAdmin, loading } = useAuthor()
 	const navigate = useNavigate()
@@ -143,7 +145,10 @@ const UserTable = () => {
 		<div className="p-6 bg-gray-50 min-h-screen">
 			<div className="max-w-7xl mx-auto">
 				<h1 className="text-3xl font-bold mb-6 text-rayonblue">Gestion des Utilisateurs</h1>
-
+				<button 
+					className='text-white bg-rayonorange ml-[77%] mb-3 w-[23%] 	rounded-lg p-2'
+					onClick={() => setModalOpen(true)}
+					>Inscrire un utilisateur</button>
 				<input
 					type="text"
 					placeholder="🔍 Rechercher par nom, email, téléphone..."
@@ -450,6 +455,12 @@ const UserTable = () => {
 					)}
 				</div>
 			</div>
+			<AddUserModal
+                isOpen={modalOpen}
+                onClose={() => setModalOpen(false)}
+				onSubmit={() => console.log()}
+
+            />
 		</div>
 	);
 };
