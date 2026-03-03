f82672db931e13bc42323e69fb085616cd26fb89
dallatIkes <samy.chaabi0@gmail.com>
2026-03-02 23:31:21 -0500

    hotfix: removed hidden images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -124,14 +124,14 @@ function Home() {
                                 Se Connecter
                             </button>
                         </div>
-                        <div className="hidden lg:flex-1 relative">
+                        <div className="lg:flex-1 relative">
                             <div className="relative z-10 transform hover:scale-105 transition-transform duration-500">
                                 <img
                                     src={orangeWoman}
                                     className="w-full h-auto max-w-lg xl:max-w-xl mx-auto drop-shadow-2xl"
                                     alt="Woman in orange"
                                 />
-
+                                {console.log(orangeWoman)}
                             </div>
                         </div>
                     </div>
